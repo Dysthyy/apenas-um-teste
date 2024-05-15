f38501fda612f8af435092c9b9852cfd4d419c82
Dysthyy <94759191+Dysthyy@users.noreply.github.com>
2024-05-15 10:48:37 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,26 @@
 let canvas;
+let isDrawing = false;
 
 function setup() {
-    canvas = createCanvas(400, 400); // Cria uma lousa de 400x400 pixels
-    canvas.parent('board'); // Define o pai do canvas como o elemento com o id "board"
-    background(0); // Define o fundo preto
+    canvas = createCanvas(400, 400);
+    canvas.parent('board');
+    background(0);
 }
 
 function draw() {
-    if (mouseIsPressed) { // Se o mouse estiver pressionado
-        stroke(255); // Cor do traço branca
-        strokeWeight(5); // Espessura do traço
-        line(pmouseX, pmouseY, mouseX, mouseY); // Desenha uma linha do último ponto ao ponto atual
+    if (isDrawing && mouseButton === LEFT) {
+        stroke(255);
+        strokeWeight(5);
+        line(pmouseX, pmouseY, mouseX, mouseY);
     }
+}
+
+function mousePressed() {
+    if (mouseButton === LEFT) {
+        isDrawing = true;
+    }
+}
+
+function mouseReleased() {
+    isDrawing = false;
 }
