671aec54e79c21e9d524019e3000e96a7ae2e474
Dysthyy <94759191+Dysthyy@users.noreply.github.com>
2024-05-15 11:00:00 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,49 @@
-let canvas;
-let isDrawing = false;
+/*
+ * ==================================================================
+ *
+ *  Esqueleto de um script JavaScript
+ *
+ * ==================================================================
+*/
 
-function setup() {
-    canvas = createCanvas(400, 400);
-    canvas.parent('board');
-    background(0);
-}
+// A `main()` só deve ser executada quando tudo estiver carregado
+window.onload = main();
 
-function draw() {
-    if (isDrawing && mouseButton === LEFT) {
-        stroke(255);
-        strokeWeight(5);
-        line(pmouseX, pmouseY, mouseX, mouseY);
-    }
-}
+// Lista de variáveis globais
+var ctx;  // contexto com a API para desenhar
 
-function mousePressed() {
-    if (mouseButton === LEFT) {
-        isDrawing = true;
-    }
-}
+/**
+ * função principal
+ */
+function main() {
+    // veja o canvas id definido no arquivo index.html
+    const canvas = document.getElementById('meucanvas');
+    // vamos definir um contexto para desenhar em 2D
+    ctx = canvas.getContext('2d');
+    if (!ctx) alert("Não consegui abrir o contexto 2d :-( ");
 
-function mouseReleased() {
-    isDrawing = false;
-}
+    let cor = 'blue';
+    desenheRect( cor, 20, 40, 160, 80 );
+    cor = 'red';
+    desenheRect( cor, 100, 60, 340, 280 );
+    cor = 'green';
+    desenheRect( cor, 320, 240, 300, 220 );
+};
+
+// ==================================================================
+//   outras funções
+
+/**
+ * recebe uma cor e os parâmetros de um retângulo e
+ * desenha a região interna do retângulo com cor.
+ * @param {string} cor 
+ * @param {number} left - coluna esquerda
+ * @param {number} top  - linha superior
+ * @param {number} width - largura do retângulo
+ * @param {number} height - altura
+ */
+function desenheRect( cor, left, top, width, height ) {
+    console.log("Desenhando retângulo ", cor);
+    ctx.fillStyle = cor;
+    ctx.fillRect( left, top, width, height );
+};
