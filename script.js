098f728828de773804b790cab832f691db807ee9
Dysthyy <94759191+Dysthyy@users.noreply.github.com>
2024-05-16 13:53:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,45 @@
           ctx.fillStyle = color;
           ctx.fill();
   }
+
+  function drawLine(x, y, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x, y2);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size * 2;
+    ctx.stroke();
+  }
+
+  increaseBtn.addEventListener("click", () => {
+    size += 5;
+    
+    if (size > 50){
+      size = 50;
+    }
+    
+    updateSizeOnScreen();
+  });
+
+  decreaseBtn.addEventListener("click", () => {
+    size -= 5;
+    
+    if (size < 5) {
+      size = 5;
+    }
+    
+    updateSizeOnScreen();
+  });
+
+
+  colorEl.addEventListener("change", (e) => {
+    color = e.target.value;
+  });
+
+  clear.addEventListener("click", (e) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  function updateSizeOnScreen() {
+    sizeEl.innerText = size;
+  }
